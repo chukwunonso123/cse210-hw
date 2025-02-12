305b9b7a0cefa0d46f23a02dd4fe525157cc73f2
chukwunonso <onyewuchichukwunonso@gmail.com>
2025-02-13 00:00:38 +0100

    week05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,80 @@
 using System;
 
-class Program
+namespace Homework
 {
-    static void Main(string[] args)
+    // Base class for all assignments
+    public class Assignment
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        private string _studentName;
+        private string _topic;
+
+        public Assignment(string studentName, string topic)
+        {
+            _studentName = studentName;
+            _topic = topic;
+        }
+
+        public string GetSummary()
+        {
+            return $"{_studentName} - {_topic}";
+        }
+
+        public string GetStudentName()
+        {
+            return _studentName;
+        }
+    }
+
+    // MathAssignment class
+    public class MathAssignment : Assignment
+    {
+        private string _textbookSection;
+        private string _problems;
+
+        public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+            : base(studentName, topic)
+        {
+            _textbookSection = textbookSection;
+            _problems = problems;
+        }
+
+        public string GetHomeworkList()
+        {
+            return $"Section {_textbookSection} Problems {_problems}";
+        }
+    }
+
+    // WritingAssignment class
+    public class WritingAssignment : Assignment
+    {
+        private string _title;
+
+        public WritingAssignment(string studentName, string topic, string title)
+            : base(studentName, topic)
+        {
+            _title = title;
+        }
+
+        public string GetWritingInformation()
+        {
+            return $"{_title} by {GetStudentName()}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+            Console.WriteLine(assignment.GetSummary());
+
+            MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+            Console.WriteLine(mathAssignment.GetSummary());
+            Console.WriteLine(mathAssignment.GetHomeworkList());
+
+            WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+            Console.WriteLine(writingAssignment.GetSummary());
+            Console.WriteLine(writingAssignment.GetWritingInformation());
+        }
     }
 }
